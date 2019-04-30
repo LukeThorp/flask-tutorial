05f5eb53e7af83aad2bfccdc6bfa5e435fcc7a60
Luke <luke.thorp@bbc.co.uk>
2019-04-30 13:47:44 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,13 @@
 # flask_app.py
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/api")
+def api():
+    data = {"some":"json"}
+    return jsonify(data)
